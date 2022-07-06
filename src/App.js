3100070e9de7ee9cd126aa6df5e7aa9aff46a925
Nico Gomez <nicolasgomez1994@gmail.com>
2022-07-06 12:12:36 -0400

    created firestore queries for scores and populate questions collection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Welcome from "./components/Welcome";
+import Home from "./components/tempComponents/Home";
+import CreateScore from "./components/tempComponents/CreateScore";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/welcome" element={<Welcome />} />
+        <Route path="/createScore" element={<CreateScore />} />
       </Routes>
     </Router>
   );

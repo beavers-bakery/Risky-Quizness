d9a7d17c0ad2c1ce529d00b22ce5b3fd3d5eeda4
Bathsheba Omidiora <shebadiora@gmail.com>
2022-07-06 10:52:19 -0500

    homepage template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Welcome from "./components/Welcome";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/welcome" element={<Welcome />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,44 @@
+import { useAuth } from '../contexts/AuthContext';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  const { user, logout } = useAuth();
+
+  return (
+    <>
+      <div className='relative flex h-screen w-screen flex-col bg-[#63B4D1] md:items-center md:justify-center md:bg-[#00CC99]'>
+        <div>
+          <Link to='/profile'>
+            <h2 className='text-center mb-4 w-full rounded bg-[#90FCF9] py-3 font-semibold'>
+              Profile
+            </h2>
+          </Link>
+          <strong>Email:</strong> {user?.email}
+          <hr />
+          <strong>Best Score:</strong> {user?.email}
+          <hr />
+          <strong>Current Score:</strong> {user?.email}
+          <button
+            type='button'
+            className='w-full rounded bg-[#90FCF9] py-3 font-semibold'
+          >
+            Start Game
+          </button>
+        </div>
+        <div className='w-100 text-center mt-2'>
+          <Link to='/welcome'>
+            <button
+              type='button'
+              className='w-full rounded bg-[#90FCF9] py-3 font-semibold md:justify-self-end'
+              onClick={logout}
+            >
+              Log Out
+            </button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;

b83d867eff6733c7d6cf7dbf3bde0bc509372519
Nico Gomez <nicolasgomez1994@gmail.com>
2022-07-08 12:05:57 -0400

    implemented results page and add user score to firestore

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,34 +1,33 @@
-import { useState } from 'react';
-import {
-  collection,
-  orderBy,
-  query,
-  where,
-  limit,
-} from 'firebase/firestore';
-import Table from './Table';
-import { db } from '../firebase';
+import { useEffect, useState } from "react";
+import { collection, orderBy, query, where, limit } from "firebase/firestore";
+import Table from "./Table";
+import { db } from "../firebase";
+import { useLocation } from "react-router-dom";
+import { addUserScore } from "../contexts/StoreContext";
+import { useAuth } from "../contexts/AuthContext";
 
 const Result = () => {
-  const [finalResult, setFinalResult] = useState(null);
-  const scoresRef = collection(db, 'scores');
+  const { state } = useLocation();
+  const { points } = state;
+  const { user } = useAuth();
+  const scoresRef = collection(db, "scores");
   const scores = query(
     scoresRef,
-    where('scores', '!=', false),
-    orderBy('scores', 'desc'),
+    where("scores", "!=", false),
+    orderBy("scores", "desc"),
     limit(3)
   );
 
+  useEffect(() => {
+    addUserScore(user?.uid, points, "General");
+  }, []);
+
   return (
-    <div className='h-screen w-screen bg-[#00CC99]'>
-      <div className='flex flex-col items-center bg-[#00CC99]'>
-        <h1 className='font-serif text-red-200 mt-10'>Results</h1>
-        {finalResult === null ? (
-          <></>
-        ) : (
-          <h2>Your Final Score is {finalResult}</h2>
-        )}
-        <div className='m-10 items-center'>
+    <div className="h-screen w-screen bg-[#00CC99]">
+      <div className="flex flex-col items-center bg-[#00CC99]">
+        <h1 className="font-serif text-red-200 mt-10">Results</h1>
+        {points === null ? <></> : <h2>Your Final Score is {points}</h2>}
+        <div className="m-10 items-center">
           <Table scores={scores} />
         </div>
       </div>
